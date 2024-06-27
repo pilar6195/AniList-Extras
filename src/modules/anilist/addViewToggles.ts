3fa795c8025a04d65d38edaa608a1600f6731467
pilar6195 <pilar6195@pilar.moe>
2024-06-27 14:02:39 -0500

    refactor: moved view toggle helper to module file.
    This is only ever used here and probably won't ever be used elsewhere.

--- src/modules/anilist/addViewToggles.ts
+++ src/modules/anilist/addViewToggles.ts
@@ -1,5 +1,10 @@
-import { $, waitFor, createElement, addViewToggle } from '@/utils/Helpers';
+import { $, $$, waitFor, createElement, addStyles } from '@/utils/Helpers';
 import { registerModule, getModule, activeModules, ModuleTags } from '@/utils/ModuleLoader';
+import SettingsManager from '@/utils/Settings';
+
+import addViewTogglesStyles from '@/styles/addViewToggles.scss';
+
+addStyles(addViewTogglesStyles);
 
 // This is a dummy module that is used to add the setting for the view toggles.
 // We have to do this since this file consists of multiple modules and I don't
@@ -19,6 +24,7 @@ registerModule.anilist({
 });
 
 const viewSwitchModule = getModule.anilist('addViewToggles')!;
+const ModuleSettings = new SettingsManager('addViewToggles');
 
 registerModule.anilist({
 	id: 'addCharactersViewSwitch',
@@ -121,3 +127,105 @@ registerModule.anilist({
 		addViewToggle('.media-staff .link', '.media-staff .grid-wrap');
 	},
 });
+
+/**
+ * Add a view toggle to the provided containers and targets.
+ */
+function addViewToggle(containers: string, targets: string) {
+	const containerElements = $$(containers).filter(c => !c.querySelector('.alextras--view-switch-toggle'));
+	const targetElements = $$(targets).filter(t => !t.classList.contains('alextras--view-switch-target'));
+
+	if (!containerElements.length || !targetElements.length) return;
+
+	for (const container of containerElements) {
+		const viewSwitch = createElement('div', {
+			attributes: {
+				class: 'alextras--view-switch-toggle',
+			},
+			// https://github.com/FortAwesome/Font-Awesome/blob/master/LICENSE.txt
+			innerHTML: `
+				<span class="alextras--switch-grid">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon svg-inline--fa fa-th-large fa-w-16">
+						<path fill="currentColor" d="M296 32h192c13.255 0 24 10.745 24 24v160c0 13.255-10.745 24-24 24H296c-13.255
+						0-24-10.745-24-24V56c0-13.255 10.745-24 24-24zm-80 0H24C10.745 32 0 42.745 0 56v160c0 13.255 10.745 24 24
+						24h192c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24zM0 296v160c0 13.255 10.745 24 24 24h192c13.255
+						0 24-10.745 24-24V296c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 24zm296 184h192c13.255 0 24-10.745
+						24-24V296c0-13.255-10.745-24-24-24H296c-13.255 0-24 10.745-24 24v160c0 13.255 10.745 24 24 24z" />
+					</svg>
+				</span>
+
+				<span class="alextras--switch-list">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon svg-inline--fa fa-th-list fa-w-16">
+						<path data-v-75ee63fe="" fill="currentColor" d="M149.333 216v80c0 13.255-10.745 24-24 24H24c-13.255
+						0-24-10.745-24-24v-80c0-13.255 10.745-24 24-24h101.333c13.255 0 24 10.745 24 24zM0 376v80c0 13.255
+						10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24
+						24zM125.333 32H24C10.745 32 0 42.745 0 56v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745
+						24-24V56c0-13.255-10.745-24-24-24zm80 448H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H205.333c-13.255
+						0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zm-24-424v80c0 13.255 10.745 24 24 24H488c13.255 0 24-10.745
+						24-24V56c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24zm24 264H488c13.255 0 24-10.745
+						24-24v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24z" />
+					</svg>
+				</span>
+			`,
+			appendTo: container,
+		});
+
+		viewSwitch.querySelector('.alextras--switch-grid')!.addEventListener('click', (event) => {
+			for (const el of $$('.alextras--view-switch-toggle .alextras--active')) {
+				el.classList.remove('alextras--active');
+			}
+
+			for (const el of $$('.alextras--view-switch-toggle .alextras--switch-grid')) {
+				el.classList.add('alextras--active');
+			}
+
+			for (const el of $$('.alextras--view-switch-target')) {
+				el.style.gridTemplateColumns = '';
+			}
+
+			ModuleSettings.set('view', 'grid');
+			event.stopPropagation();
+		});
+
+		viewSwitch.querySelector('.alextras--switch-list')!.addEventListener('click', (event) => {
+			for (const el of $$('.alextras--view-switch-toggle .alextras--active')) {
+				el.classList.remove('alextras--active');
+			}
+
+			for (const el of $$('.alextras--view-switch-toggle .alextras--switch-list')) {
+				el.classList.add('alextras--active');
+			}
+
+			for (const el of $$('.alextras--view-switch-target')) {
+				el.style.gridTemplateColumns = '1fr';
+			}
+
+			ModuleSettings.set('view', 'list');
+			event.stopPropagation();
+		});
+	}
+
+	for (const target of targetElements) {
+		target.classList.add('alextras--view-switch-target');
+	}
+
+	switch (ModuleSettings.get('view')) {
+		case 'list': {
+			for (const s of $$('.alextras--view-switch-toggle .alextras--switch-list')) {
+				s.classList.add('alextras--active');
+			}
+
+			for (const t of $$('.alextras--view-switch-target')) {
+				t.style.gridTemplateColumns = '1fr';
+			}
+
+			break;
+		}
+
+		default: {
+			for (const s of $$('.alextras--view-switch-toggle .alextras--switch-grid')) {
+				s.classList.add('alextras--active');
+			}
+		}
+	}
+}
